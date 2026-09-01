4f4cc1316f0f9c9c9db8dd7a7bd238b8aa4a0439
Spoocyy <erobertson4848@gmail.com>
2026-08-31 21:54:28 -0400

    added rotation and movement to camera look script

--- Team1-Proj1/Assets/Scripts/CameraLook.cs
+++ Team1-Proj1/Assets/Scripts/CameraLook.cs
@@ -5,15 +5,40 @@ using UnityEngine;
 
 public class CameraLook : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] Transform playerBody;
+    [SerializeField] float sensitivity = 0.1f;
+    [SerializeField] float minPitch = -80f;
+    [SerializeField] float maxPitch = 80f;
+
+    private PlayerControls input;
+    private Vector2 lookInput;
+    private float pitch;
+
+    private void Awake()
     {
-        
+        input = GetComponent<PlayerControls>();
+        input.Player.Look.performed += ctx => lookInput = ctx.ReadValue<Vector2>();
+        input.Player.Look.canceled += ctx => lookInput = Vector2.zero;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LateUpdate()
     {
-        
+        float yaw = lookInput.x * sensitivity;
+        pitch -= lookInput.y * sensitivity;
+        pitch = Mathf.Clamp(pitch, minPitch, maxPitch);
+
+        playerBody.Rotate(Vector3.up * yaw);
+
+        transform.localRotation = Quaternion.Euler(pitch, 0f, 0f);
+    }
+
+    private void OnEnable()
+    {
+        input.Enable();
+    }
+
+    private void OnDisable()
+    {
+        input.Disable();
     }
 }
