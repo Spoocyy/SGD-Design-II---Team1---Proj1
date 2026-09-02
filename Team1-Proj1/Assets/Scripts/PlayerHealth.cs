72530ab0ad6de4ed1ce369d6f5617b42babc05a2
Spoocyy <erobertson4848@gmail.com>
2026-09-02 00:38:24 -0400

    added code to player health

--- Team1-Proj1/Assets/Scripts/PlayerHealth.cs
+++ Team1-Proj1/Assets/Scripts/PlayerHealth.cs
@@ -2,15 +2,41 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] int maxHealth = 100;
+    [SerializeField] HealthUI healthUI;
+    [SerializeField] PlayerRespawn playerRespawn;
+
+    public int CurrentHealth { get; private set; }
+
+    private void Awake()
     {
-        
+        CurrentHealth = maxHealth;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        healthUI.UpdateBar(CurrentHealth, maxHealth);
+    }
+
+    public void TakeDamage(int amount)
+    {
+        if (CurrentHealth <= 0)
+        {
+            return;
+        }
+
+        CurrentHealth = Mathf.Max(CurrentHealth - amount, 0);
+        healthUI.UpdateBar(CurrentHealth, maxHealth);
+
+        if (CurrentHealth == 0)
+        {
+            playerRespawn.HandleDeath();
+        }
+    }
+
+    public void ResetHealth()
+    {
+        CurrentHealth = maxHealth;
+        healthUI.UpdateBar(CurrentHealth, maxHealth);
     }
 }
