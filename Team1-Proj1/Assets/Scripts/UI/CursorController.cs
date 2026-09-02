0d602a4afd5ea7be3b668a1e3fb0276e405d1d32
Spoocyy <erobertson4848@gmail.com>
2026-09-02 02:46:53 -0400

    created pause menu and cursor controller script and added headers

---
+++ Team1-Proj1/Assets/Scripts/UI/CursorController.cs
@@ -0,0 +1,19 @@
+//Erik Robertson
+//9/1/2026
+//SGD Design II - Project 1 - Team 1
+using UnityEngine;
+
+public class CursorController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
