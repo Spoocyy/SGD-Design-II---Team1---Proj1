b244f3af40963f3fd03d36ffea3cf2715f69a6b5
Spoocyy <erobertson4848@gmail.com>
2026-09-02 03:48:18 -0400

    fixed level transition

--- Team1-Proj1/Assets/Scripts/Objects/LevelExit.cs
+++ Team1-Proj1/Assets/Scripts/Objects/LevelExit.cs
@@ -1,16 +1,23 @@
+//Erik Robertson
+//9/2/2026
+//SGD Design II - Project 1 - Team 1
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelExit : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void LoadNextScene()
     {
-        
+        Time.timeScale = 1.0f;
+        int nextIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        SceneManager.LoadScene(nextIndex);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.CompareTag("Player"))
+        {
+            LoadNextScene();
+        }
     }
 }
