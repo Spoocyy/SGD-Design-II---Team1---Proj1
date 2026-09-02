af9631f575864b548878a275d3a05da77299a8a1
Spoocyy <erobertson4848@gmail.com>
2026-09-02 03:49:29 -0400

    renamed level exit script to level transition

--- Team1-Proj1/Assets/Scripts/Objects/LevelTransition.cs
+++ Team1-Proj1/Assets/Scripts/Objects/LevelTransition.cs
@@ -4,7 +4,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LevelExit : MonoBehaviour
+public class LevelTransition : MonoBehaviour
 {
     public void LoadNextScene()
     {
