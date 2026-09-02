57136d1bb22c99f047745121f3441ca82cc04e89
Spoocyy <erobertson4848@gmail.com>
2026-09-02 19:54:20 -0400

    added code to audio manager

--- Team1-Proj1/Assets/Scripts/AudioManager.cs
+++ Team1-Proj1/Assets/Scripts/AudioManager.cs
@@ -1,16 +1,42 @@
+//Erik Robertson
+//9/2/2026
+//SGD Design II - Project 1 - Team 1
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static AudioManager instance;
+    [SerializeField] AudioSource sfxSource;
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource ambienceSource;
+
+    private void Awake()
     {
-        
+        if(instance == null) { instance = this; }
+        else Destroy(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlaySFX(AudioClip clip)
     {
-        
+        sfxSource.PlayOneShot(clip);
     }
+
+    public void PlayMusic(AudioClip clip)
+    {
+        if(musicSource.clip == clip && musicSource.isPlaying) { return; }
+
+        musicSource.clip = clip;
+        musicSource.loop = true;
+        musicSource.Play();
+    }
+
+    public void PlayAmbience(AudioClip clip)
+    {
+        ambienceSource.clip = clip;
+        ambienceSource.loop = true;
+        ambienceSource.Play();
+    }
+
+    public void StopMusic() => musicSource.Stop();
+    public void StopAmbience() => ambienceSource.Stop();
 }
